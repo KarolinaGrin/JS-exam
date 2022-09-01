3446c4b3c9ba6969d885a4cd49e5d8231ea2b13a
Karolina Grin <karolina.griniute@codeacademylt.omicrosoft.com>
2022-09-01 22:01:50 +0300

    8 užduotis testavimas su adder

--- Task 8/script.js
+++ Task 8/script.js
@@ -5,3 +5,8 @@ subtraction(a, b) - priima du skaičius ir grąžina jų skirtumą.
 multiplication(a, b) - priima du skaičius ir grąžina jų daugybos rezultatą;
 division(a, b) - priima du skaičius ir grąžina jų dalybos rezultatą;
 ------------------------------------------------------------------------------------ */
+
+
+const adder = new Function('a', 'b', 'return a + b')
+
+adder (2, 6);
